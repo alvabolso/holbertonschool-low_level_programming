1bad4622c6e41bc48d7901720aaf8aaa139174b7
alvabolso <alvabolso@gmail.com>
2022-02-04 07:28:24 -0800

    uper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z' ; letra++)
+	for (letra = "A"; letra <= "Z" ; letra++)
+	putchar(letra);
+	putchar('\n');
+	return (0);
+}
