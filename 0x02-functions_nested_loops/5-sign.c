0872e707f8e4fea0f81c83fb99417dd4e285b402
alvabolso <alvabolso@gmail.com>
2022-02-07 18:23:52 -0800

    sadsd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,11 +12,11 @@ int print_sign(int n)
 		return (0);
 			}
 	else if (n > 0)
-						{
-											_putchar ('+');
-														return (1);
+		{
+		_putchar ('+');
+		return (1);
 																}
-							else
+	else
 											{
 															_putchar ('-');
 																		return (-1);
