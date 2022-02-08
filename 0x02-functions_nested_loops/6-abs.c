7914acbc72cb8c7efb4774d77c2095bcefd54598
alvabolso <alvabolso@gmail.com>
2022-02-07 18:28:48 -0800

    sadsa

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  * _abs - absolute value
+ *   * @r : int
+ *    * Return: abs
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+	return (r);
+	}
+	else
+	{
+	r = (r * (-1));
+	return (r);
+															}
+	}
