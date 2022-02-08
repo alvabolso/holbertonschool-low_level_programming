5fa20f173c55b75867000ef9ce8f0615f8f35a6d
alvabolso <alvabolso@gmail.com>
2022-02-07 18:31:56 -0800

    sads

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+}
+#include "main.h"
+/**
+ *  *print_last_digit - print lastdigit
+ *   *@n : int
+ *    *Return: last digit
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	if (n < 0)
+	{
+		n = -n;
+	}
+	_putchar(n + '0');
+	return (n);
+}
