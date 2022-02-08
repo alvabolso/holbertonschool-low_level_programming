1f72f14d419eb65878f78b294cd4d46d0e03ba16
alvabolso <alvabolso@gmail.com>
2022-02-07 17:50:12 -0800

    sads

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
+
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+ * *print_alphabet - check the code
+ * *
+ * * Return: Always 0.
+ * */
+void print_alphabet(void)
 {
-	char letter;
+	char c;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
-
-	_putchar('\n');
-	return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+	_putchar ('\n');
 }
