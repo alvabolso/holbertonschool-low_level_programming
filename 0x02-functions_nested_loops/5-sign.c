b39aeba29e21f554ca2e024b8023814ef59523f5
alvabolso <alvabolso@gmail.com>
2022-02-07 18:20:18 -0800

    sad

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  * print_sign - sign of a number
+ *   * @n : input nuber
+ *    * Return: 1 if positive, zero if =0 or -1 if negative
+ */
+int print_sign(int n)
+{
+		if (n == 0)
+				{
+							_putchar ('0');
+									return (0);
+											}
+				else if (n > 0)
+							{
+											_putchar ('+');
+														return (1);
+																}
+							else
+											{
+															_putchar ('-');
+																		return (-1);
+																					}
+											}
