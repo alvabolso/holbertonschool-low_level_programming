287123d61fe0bc80667500fcdf279ba0a8f44787
alvabolso <alvabolso@gmail.com>
2022-02-07 18:35:44 -0800

    2safasdf

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * *jack_bauer - check the code
+ * *Return: Always 0.
+*/
+void jack_bauer(void)
+{
+	int one;
+	int four;
+	int print;
+
+	for (one = 00; one <= 23; one++)
+	{
+		for (four = 00; four <= 59; four++)
+		{
+			print = one / 10;
+			_putchar (print + '0');
+			print = one % 10;
+			_putchar (print + '0');
+			_putchar (':');
+			print = four / 10;
+			_putchar(print + '0');
+			print = four % 10;
+			_putchar(print + '0');
+			_putchar('\n');
+		}
+	}
+}
