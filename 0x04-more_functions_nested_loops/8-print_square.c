134743ce787bc6bd976322ec991518011217361d
alvabolso <alvabolso@gmail.com>
2022-02-09 14:58:40 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,25 +7,23 @@
  *     *
  *      * Return: void
  */
-
 void print_square(int size)
 {
-		int fil;
-			int col;
+	int fil;
+	int col;
 
-				if (size > 0)
-						{
-									for (fil = 0; fil < size; fil++)
-												{
-																for (col = 0; col < size; col++)
-																				{
-																									_putchar('#');
-																												}
-																			_putchar('\n');
-																					}
-										}
-					else
-							{
-										_putchar('\n');
-											}
-}
+	if (size > 0)
+	{
+		for (fil = 0; fil < size; fil++)
+		{
+			for (col = 0; col < size; col++)
+			{
+																			_putchar('#');
+																		}
+																		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
