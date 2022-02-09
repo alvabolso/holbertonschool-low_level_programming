fb1589c3e14bc43b279a28162a7b0c6ce0a144be
alvabolso <alvabolso@gmail.com>
2022-02-09 09:21:50 -0800

    linea

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  * print_diagonal - prints a n length \ diagonal line
+ *   * @n: length of line
+ *    * Description: Same as above
+ *     *
+ *      * Return: void
+ */
+void print_diagonal(int n)
+{
+	int linea = 0;
+	int space;
+
+	if (n > 0)
+	{
+		while (linea < n)
+		{
+		for (space = 0; space < linea; space++)
+		{
+		_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		linea++;
+		}
+	}
+	else
+		{
+		_putchar('\n');
+		}
+}	
+		
