68c7f125f69389a063d1764cc0fb7742fd2f5312
alvabolso <alvabolso@gmail.com>
2022-02-09 15:31:09 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_triangle - print triangle
+ *@size : size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int space;
+	int hash;
+	int line;
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+	for (line = 1; line <= size; line++)
+	{
+	for (space = 1; space <= size - line; space++)
+	{
+	_putchar(' ');
+	}
+	for (hash = 1; hash <= line; hash++)
+	{
+	_putchar('#');
+	}
+	_putchar(10);
+	}
+	}
+}
