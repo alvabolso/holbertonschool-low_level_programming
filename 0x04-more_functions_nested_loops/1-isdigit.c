eb8678eff6bd7f99520e7d55f8e4f1922d41cbc3
alvabolso <alvabolso@gmail.com>
2022-02-09 06:28:35 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * _isupper - Prints 1 if the letter is uppercase
+ *   * and if not print 0
+ *    * @letra: letra is an ASCII character
+ *     * Description: Same as above
+ *      *
+ *    
+ *
+ */
+int _isdigit(int c)
+{
+		if (c >= '0' && c <= '9')
+		{
+		return (1);
+		}
+		else
+		{		
+		return (0);
+		}
+	}
