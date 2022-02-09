a7a3d903734d82b9e72792e126cc9f6e8f78b107
alvabolso <alvabolso@gmail.com>
2022-02-09 07:16:36 -0800

    printnum

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  
+ *  
+ * 
+ * multiplies two integers.   
+ *
+ */
+int mul(int a, int b)
+	{
+	return (a * b);
+	}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_numbers - Prints 0-9 numbers
+ *   * Description: Same as above
+ *    *
+ *     * Return: void
+ *      */
+
+void print_numbers(void)
+{	
+	int num;
+
+	for (num = 0; num < 10 ; num++)
+	_putchar((num % 10) + '0');
+	_putchar('\n');
+	}
