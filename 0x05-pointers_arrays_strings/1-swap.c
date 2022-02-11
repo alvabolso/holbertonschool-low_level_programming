26e4b74ca8708a3aaf99c1219a175e476b9909d8
alvabolso <alvabolso@gmail.com>
2022-02-11 07:52:01 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - check the code
+ * @a: number1
+ * @b: number2
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	 *a = *b;
+	 *b = *a;
+
+}
