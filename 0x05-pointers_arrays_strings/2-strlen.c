ecd33548fde89c7205e32e51d0c3978f774b5cdd
alvabolso <alvabolso@gmail.com>
2022-02-11 09:39:34 -0800

    lenght

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,19 @@
 include "main.h"
+
 /**
  * _strlen - returns the length of a string
  * @s: string
  * Return: length of the string
  */
+
 int _strlen(char *s)
 {
-	int le = 0;
+	int length = 0;
 
-	while (s[le] != '\0')
+	while (s[length] != '\0')
 	{
-		le++;
+		length++;
 	}
 
-	return (le);
+	return (length);
 }
