f5bbc9f73d806810457c78c417bd65997a7bc052
alvabolso <alvabolso@gmail.com>
2022-02-21 07:16:04 -0800

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,17 +2,28 @@
 #define NULL (void *)0
 
 /**
- *  * _strchr - locates a char in a string
- *   * @s: string that can contain c
- *    * @c: char to be found
- *     * Return: pointer to the first c ocurrence
- *      * or NULL if c is not foudn at s
- *       */
+ * _strchr - locates a char in a string
+ * @s: string that can contain c
+ * @c: char to be found
+ * Return: pointer to the first c ocurrence
+ * or NULL if c is not foudn at s
+ */
+
 char *_strchr(char *s, char c)
 {
-do      {
-        if (*s == c) return s;
-        } while (*s++) ;
-return NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return (NULL);
 }
 
