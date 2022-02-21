746166e9773392eb79f3507e6955632e2bd9cbd3
alvabolso <alvabolso@gmail.com>
2022-02-21 07:13:26 -0800

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,13 @@
 #include "main.h"
-#include <stdio.h>
+#define NULL (void *)0
+
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- **/
+ *  * _strchr - locates a char in a string
+ *   * @s: string that can contain c
+ *    * @c: char to be found
+ *     * Return: pointer to the first c ocurrence
+ *      * or NULL if c is not foudn at s
+ *       */
 char *_strchr(char *s, char c)
 {
 do      {
