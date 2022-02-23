f726d880a2ed5671d7ec0e33241e2451bf077c16
alvabolso <alvabolso@gmail.com>
2022-02-23 08:20:08 -0800

    task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * Return: return
+ * @n: variable
+ */
+int _sqrt_recursion(int n)
+{
+	int x, y;
+
+	if (n < 0)
+	{
+		return (0);
+	}
+	else if (n < 2)
+	{
+		return (n);
+	}
+	else
+	{
+		y = _sqrt_recursion(n >> 2) << 1;
+		x = y + 1;
+
+	if (x * x > n)
+	{
+		return (y);
+	}
+	else
+	{
+		return (x);
+	}
+	}
+}
