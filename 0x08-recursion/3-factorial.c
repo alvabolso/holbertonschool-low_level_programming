6d4a7bcfd0818f26337da1ef92241ddce3f3519e
alvabolso <alvabolso@gmail.com>
2022-02-23 07:43:20 -0800

    task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,16 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
+	if (n == 0)
+	{
+	return (1);
+	}
+	else if (n != 0 && n > 0)
+	{
+	return (n * factorial(n - 1));
+	}
+	else
 	{
 	return (-1);
 	}
-	return (n * factorial(n - 1));
 }
