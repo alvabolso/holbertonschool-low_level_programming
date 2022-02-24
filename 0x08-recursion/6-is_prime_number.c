c5d62d2bf67017c659d612b4df3f254153958a2d
alvabolso <alvabolso@gmail.com>
2022-02-24 06:17:04 -0800

    task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,10 +4,7 @@
  * prime_number - auxiliar function for is_prime_number
  * @n: number to check
  * @divisor: divisor
- * Return: prime number or not
- * is_prime_number - check if n is a prime number, it use prime_number function
- * @n: number to check
- * Return: 1 if it is prime, 0 if it is not
+ * Return: prime number or not 
  */
 
 int prime_number(int n, int divisor)
@@ -19,6 +16,11 @@ int prime_number(int n, int divisor)
 	return (prime_number(n, divisor + 1));
 }
 
+/**
+ * is_prime_number - check if n is a prime number, it use prime_number function
+ * @n: number to check
+ * Return: 1 if it is prime, 0 if it is not
+ */
 int is_prime_number(int n)
 {
 	int divisor = 2;
