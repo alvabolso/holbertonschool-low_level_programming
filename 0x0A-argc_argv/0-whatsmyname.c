2fe5c806e3c3b3f054f47a30e740d88e8a9a81f7
alvabolso <alvabolso@gmail.com>
2022-02-25 16:08:09 -0800

    tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,18 +12,4 @@ int main(int argc, char *argv[])
 	if (argc > 0)
 		printf("%s\n", argv[0]);
 	return (0);
-}#include "main.h"
-
-/**
- * main - prints a name followed by a new line
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if successful
- */
-
-int main(int argc, char *argv[])
-{
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
 }
