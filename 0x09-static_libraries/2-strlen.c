67b451e10bb6d5c4795f1969db958d43067086fe
alvabolso <alvabolso@gmail.com>
2022-03-01 15:20:09 -0800

    lib

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int le = 0;
+
+	while (s[le] != '\0')
+	{
+		le++;
+	}
+
+	return (le);
+}
