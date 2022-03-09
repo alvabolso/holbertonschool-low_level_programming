615db1205e1ad2b752de3c592959d773b04340cf
alvabolso <alvabolso@gmail.com>
2022-03-09 14:50:51 -0800

    tasks

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints the info of struct dog
+ * @d: a pointer variable of type struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age < 0)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+		(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
+	}
+	return;
+}
